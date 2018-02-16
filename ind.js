92cd78ba8f96a1653f283bee1cf5f3951eae2fe5
Sahsa29 <aeandra.butenko@gmail.com>
2018-02-16 12:14:19 +0200

    first commit

---
+++ ind.js
@@ -0,0 +1,20 @@
+
+ var v = "вгадай фрукт";
+var   b = prompt(v);
+
+if(b!== "Апельсин"){
+	console.log("не вірно");
+	console.log("він круглий");
+	  b = prompt(v);
+	  if(b!== "Апельсин"){
+	console.log("не вірно");
+	console.log("він цяточку");
+	}
+else {
+	document.write("TRUEEEE");
+}
+} else {
+	document.write("TRUEEEE");
+}
+
+  
